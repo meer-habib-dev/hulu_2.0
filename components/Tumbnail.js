ebc2e0dc540570c39a98d1f33e35de03be4cb694
meer-habib-dev <contact.freelancermir@gmail.com>
2022-01-23 02:53:00 +0600

    updated

--- components/Tumbnail.js
+++ components/Tumbnail.js
@@ -2,36 +2,39 @@ import { ThumbUpIcon } from "@heroicons/react/outline";
 import Image from "next/image";
 import React, { forwardRef } from "react";
 
-const Tumbnail = forwardRef(({ result }, ref) => {
-  const BASE_URL = "https://image.tmdb.org/t/p/original/";
-  return (
-    <div
-      ref={ref}
-      className="group p-2 transition duration-200 cursor-pointer ease-in transform sm:hover:scale-105 hover:z-50"
-    >
-      <Image
-        layout="responsive"
-        src={
-          `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
-          `${BASE_URL}${result.poster_path}`
-        }
-        alt="img"
-        height={1080}
-        width={1920}
-      />
-      <div className="p2">
-        <p className="truncate max-w-md">{result.overview}</p>
-        <h1 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
-          {result.title || result.original_name}
-        </h1>
-        <p className="flex items-center group-hover:opacity-100 opacity-0">
-          {result.media_type && `${result.media_type} .`}
-          {result.release_date || result.first_air_date}{" "}
-          <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
-        </p>
+const Tumbnail =
+  React.forwardRef <
+  HTMLInputElement >
+  (({ result }, ref) => {
+    const BASE_URL = "https://image.tmdb.org/t/p/original/";
+    return (
+      <div
+        ref={ref}
+        className="group p-2 transition duration-200 cursor-pointer ease-in transform sm:hover:scale-105 hover:z-50"
+      >
+        <Image
+          layout="responsive"
+          src={
+            `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
+            `${BASE_URL}${result.poster_path}`
+          }
+          alt="img"
+          height={1080}
+          width={1920}
+        />
+        <div className="p2">
+          <p className="truncate max-w-md">{result.overview}</p>
+          <h1 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
+            {result.title || result.original_name}
+          </h1>
+          <p className="flex items-center group-hover:opacity-100 opacity-0">
+            {result.media_type && `${result.media_type} .`}
+            {result.release_date || result.first_air_date}{" "}
+            <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
+          </p>
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  });
 
 export default Tumbnail;
